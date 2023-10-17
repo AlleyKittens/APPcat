8abdc50741802d97dc40467a155490b40089a1ec
Delania Oliveira <delania.oliveiraa@gmail.com>
2023-10-17 10:44:38 -0300

    Configuração do estado de sessão do nextauth.js

---
+++ client/src/app/providers/sessionProvider.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+interface NextAuthSessionProviderProps {
+  children: ReactNode
+}
+
+export default function NextAuthSessionProvider({ children }: NextAuthSessionProviderProps) {
+  return <SessionProvider>{children}</SessionProvider>
+}
